d2f334c064d926acccac630961317035288b0095
Divyanshu Pairhar <divyanshu1447@gmail.com>
2025-11-24 23:15:10 +0530

    control flow

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,43 @@
-// use std::isie;
+fn fibonacci(num: i32) -> i32 {
+    let mut first: i32 = 0;
+    let mut second: i32 = 1;
 
-// fn fibonacci(num: isize) -> i64 {
-//     let mut first :i32 = 0;
-//     let mut second :i32 = 1;
-
-//     let count = 1;
-//     while count != num {
-//         let temp :i64 = second.into() + first;
-//         first = second;
-//         second = temp;
-//     }
-//     return first.into();
-// }
-
-
-fn arg_pass(val: i64) -> i64{
-    let result = val + 1;
-    return result;
+    let mut count = 1;
+    while count != num {
+        let temp = first;
+        first = second;
+        second = dbg!(temp) + second;
+        count += 1;
+    }
+    return first;
 }
+fn collatz_seq(mut num: i32) -> i32 {
+    let mut count = 1;
+    loop {
+        match num {
+            1 => break,
+            _ => {
+                if num % 2 != 0 {
+                    num = 3 * num + 1;
+                    count += 1;
+                } else {
+                    num = num / 2;
+                    count += 1;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+// fn arg_pass(val: i64) -> i64{
+//     let result = val + 1;
+//     return result;
+// }
 fn main() {
     println!("Hello, Div!");
-    // let fib_ans = fibonacci(5);
-    // println!("{}", fib_ans)
+    let fib_ans = fibonacci(5);
+    println!("{fib_ans}-{}", collatz_seq(11));
 
-    let value =  10;
-    let result = arg_pass(value);
-    println!("{result} - {value}");
-    let s1 = String::from("hello");
-takes_ownership(s1); // s1's ownership is moved into the function.
-println!("{s1}")
+    // arrays
 }
-
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string); // The function now owns 'some_string'
-} // 'some_string' goes out of scope and is dropped/freed here.
-
