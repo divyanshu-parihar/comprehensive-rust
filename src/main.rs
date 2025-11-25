b32d81f5959e63d23baef14a8c90e932fe10f8f4
Divyanshu Pairhar <divyanshu1447@gmail.com>
2025-11-25 09:19:44 +0530

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 fn fibonacci(num: i32) -> i32 {
     let mut first: i32 = 0;
     let mut second: i32 = 1;
@@ -30,14 +31,51 @@ fn collatz_seq(mut num: i32) -> i32 {
     return count;
 }
 
-// fn arg_pass(val: i64) -> i64{
-//     let result = val + 1;
-//     return result;
-// }
+fn transpose(arr: &mut [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    for i in 0..3 {
+        for j in 0..3 {
+            // swap elements a[i][j] a[j][i]
+            if j <= i {
+                continue;
+            }
+            let temp = arr[i][j];
+
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+
+    return *arr;
+}
+
+fn takes_element(elems: (i32, String, &str)) {
+    // println!("{} , {}, {}",elems.0,elems.1, elems.2)
+    let (.., second, third) = elems;
+    println!("{second}-{third}")
+}
 fn main() {
     println!("Hello, Div!");
     let fib_ans = fibonacci(5);
     println!("{fib_ans}-{}", collatz_seq(11));
 
-    // arrays
+    // let mut arr = [0,1,2,3,4,5,6];
+    // arr[0] = 10;
+    // const FIRST: i32 = i32::MAX;
+    // println!("{}",FIRST + 1); // GIVES OVERFLOW ERROR
+    // println!("{arr:?}")
+
+    // 2d arrays
+    let mut twoarr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    println!("{:?}", transpose(&mut twoarr));
+
+    // take element compound elements;
+    let ster = "div";
+    takes_element((32, String::from("Divyanshu"), &ster));
+
+    let input = 'a';
+    match input {
+        'q' => println!("quitting"),
+        key if key.is_uppercase() => println!("Uppercase key"),
+        _ => println!("Default"),
+    }
 }
